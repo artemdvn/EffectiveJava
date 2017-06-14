d96890a44f471f7ca86da0db4cba104824884a72
Artem Dvornichenko <Artem.Dvornichenko@halliburton.com>
2017-06-14 13:13:35 +0300

    chapter 24 completed

---
+++ src/effectivejava/chapter5/article24/SuppressWarningsToArray.java
@@ -0,0 +1,41 @@
+package effectivejava.chapter5.article24;
+
+import java.util.Arrays;
+
+public class SuppressWarningsToArray {
+
+	private Integer[] elements;
+	private int size;
+
+	public <T> T[] toArray(T[] a) {
+		if (a.length < size) {
+			return (T[]) Arrays.copyOf(elements, size, a.getClass());
+		}
+		System.arraycopy(elements, 0, a, 0, size);
+		if (a.length > size) {
+			a[size] = null;
+		}
+		return a;
+
+	}
+
+	// Adding local variable to reduce scope of @SuppressWarnings
+	public <T> T[] toArraySuppressWarnings(T[] a) {
+
+		if (a.length < size) {
+			// This cast is correct because the array we're creating
+			// is of the same type as the one passed in, which is T[].
+			@SuppressWarnings("unchecked")
+			T[] result = (T[]) Arrays.copyOf(elements, size, a.getClass());
+			return result;
+
+		}
+		System.arraycopy(elements, 0, a, 0, size);
+		if (a.length > size) {
+			a[size] = null;
+		}
+		return a;
+
+	}
+
+}
